ccb7c910e75160855b21ba936719e34fb5b07a8a
root <martijn.totte@gmail.com>
2018-05-24 21:31:31 +0200

    Foto uploaden front end

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,6 +15,11 @@
                     @endif
 
                     You are logged in!
+                    <form action="upload.php" method="post" enctype="multipart/form-data">
+                      Select image to upload:
+                      <input type="file" name="fileToUpload" id="fileToUpload">
+                      <input type="submit" value="Upload Image" name="submit">
+                    </form>
                 </div>
             </div>
         </div>
